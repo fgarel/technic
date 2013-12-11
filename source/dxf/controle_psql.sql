c505c2fdf51430a51db4b0c56c20f7160e3441f7
Frédéric Garel <frederic.garel@gmail.com>
2013-12-11 19:08:45 +0100

    amelioration de latex

---
+++ source/dxf/controle_psql.sql
@@ -0,0 +1,4 @@
+﻿select st_asewkt(wkb_geometry) from "999_02_00047_00026_13_b_18_fp2";
+select st_asewkt(wkb_geometry) from "releve_complementaire_rue_kaestler" limit 100;
+
+--DECLARE qgisf3_0 BINARY CURSOR FOR SELECT st_asbinary(st_force2d("wkb_geometry"),'NDR'),"ogc_fid" FROM "public"."releve_complementaire_rue_kaestler"
