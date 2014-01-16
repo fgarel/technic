d84fcb3a522cbfce66e9bd23b9bef4834ba08d00
Frédéric Garel <frederic.garel@gmail.com>
2014-01-16 17:40:01 +0100

    amelioration des vues dgfip_parcelle dgfip_local et dgfip_pev

--- source/cadastre/bin/03_04_export.sh
+++ source/cadastre/bin/03_04_export.sh
@@ -3,31 +3,27 @@
 HOST=10.2.10.56
 
 #-- export des proprietaires à partir d'une vue vers un fichier csv
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from vue_001) to stdout delimiter ';';" \
-    > ../data1/fin/vue_001.txt
+#psql -d cadastre -U contrib -h $HOST -c "copy (select * from vue_001) to stdout delimiter ';';" \
+#    > ../data1/fin/vue_001.txt
 #psql -h $HOST -U contrib -d cadastre -f ./sql/export.sql
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from vue_001) to stdout delimiter ';';" \
-    > ../data1/fin/vue_001.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from vue_002) to stdout delimiter ';';" \
-    > ../data1/fin/vue_002.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_00) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_00.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_10) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_10.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_21) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_21.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_30) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_30.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_36) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_36.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_40) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_40.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_50) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_50.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from ex_bati_article_60) to stdout delimiter ';';" \
-    > ../data1/fin/ex_bati_article_60.txt
 
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from local) to stdout delimiter ';';" \
-    > ../data1/fin/local.txt
-psql -d cadastre -U contrib -h $HOST -c "copy (select * from pev) to stdout delimiter ';';" \
-    > ../data1/fin/pev.txt
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_parcelle WHERE prop_article_courant.ddenom like '%COMMUNE DE LA ROCHELLE%') to stdout delimiter ';';" \
+    > ../data1/fin/larochelle_parcelles.txt
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_local WHERE prop_article_courant.ddenom like '%COMMUNE DE LA ROCHELLE%') to stdout delimiter ';';" \
+    > ../data1/fin/larochelle_locaux.txt
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_pev WHERE prop_article_courant.ddenom like '%COMMUNE DE LA ROCHELLE%') to stdout delimiter ';';" \
+    > ../data1/fin/larochelle_pev.txt
+
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_parcelle WHERE prop_article_courant.ddenom like '%COPROP IMM 7B RUE DES FONDERIES%') to stdout delimiter ';';" \
+    > ../data1/fin/coprop_parcelles.txt
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_local WHERE prop_article_courant.ddenom like '%COPROP IMM 7B RUE DES FONDERIES%') to stdout delimiter ';';" \
+    > ../data1/fin/coprop_locaux.txt
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_pev WHERE prop_article_courant.ddenom like '%COPROP IMM 7B RUE DES FONDERIES%') to stdout delimiter ';';" \
+    > ../data1/fin/coprop_pev.txt
+
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_parcelle WHERE prop_article_courant.ddenom like '%GAREL/FREDERIC%') to stdout delimiter ';';" \
+    > ../data1/fin/garel_parcelles.txt
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_local WHERE prop_article_courant.ddenom like '%GAREL/FREDERIC%') to stdout delimiter ';';" \
+    > ../data1/fin/garel_locaux.txt
+psql -d cadastre -U contrib -h $HOST -c "copy (select * from dgfip_pev WHERE prop_article_courant.ddenom like '%GAREL/FREDERIC%') to stdout delimiter ';';" \
+    > ../data1/fin/garel_pev.txt
--- source/cadastre/bin/sql/select.sql
+++ source/cadastre/bin/sql/select.sql
@@ -129,234 +129,118 @@ WHERE "ddenom" LIKE '%GAREL/FREDERIC%';
 -- selection des proprietaires "commune de La Rochelle";
 -- et sauvegarde du resultat dans une vue
 -- qui sera facilement exportee par la suite
-CREATE VIEW vue_001 AS
-SELECT
-  ddenom,
-  dnuper,
-  dqualp,
-  dnomlp,
-  dprnlp, 
-  jdatnss
-FROM prop_article_courant
-WHERE "ddenom" LIKE '%COMMUNE DE LA ROCHELLE%';
 
--- Selection de prop_article_courant
-CREATE VIEW vue_002 AS
-select 
-  ccodep, -- code département
-  ccodir, -- code direction
-  ccocom, -- code commune INSEE
-  cgroup_signification, -- groupe de compte communal
-  dnumcp, -- numéro d’ordre de compte
-  dnulp, -- numéro de libellé partiel
-  ccocif, -- code cdif
-  dnuper, -- numéro de personne dans le cdif
-  ccodro_signification, -- code du droit réel ou parti- culier
-  ccodem_signification, -- code du démembrement/indivi-sion
-  gdesip, -- indicateur du destinataire de l’avis d’imposition
-  gtoper_signification, -- indicateur de personne physi-que ou morale
-  ccoqua_signification, -- Code qualité de personne phy-sique
-  gnexcf, -- code exo ecf
-  dtaucf, -- taux exo ecf
-  dnatpr, -- Code nature de personne physique ou morale
-  ccogrm_signification, -- Code groupe de personne morale
-  dsglpm, -- sigle de personne morale
-  dforme, -- forme juridique abrégée majic2
-  ddenom, -- Dénomination de personne physique ou morale
-  gtyp3, -- type de la 3eme ligne d’a-dresse
-  gtyp4, -- Type de la 4eme ligne d’a-dresse
-  gtyp5, -- type de la 5eme ligne d’a-dresse
-  gtyp6, -- type de la 6eme ligne d’a-dresse
-  dlign3, -- 3eme ligne d’adresse
-  dlign4, -- 4eme ligne d’adresse
-  dlign5, -- 5eme ligne d’adresse
-  dlign6, -- 6eme ligne d’adresse
-  ccopay, -- code de pays étranger et TOM
-  ccodep1a2, -- Code département de l’adresse
-  ccodira, -- Code direction de l’adresse
-  "ccocom-adr", -- Code commune de l’adresse
-  ccovoi, -- Code majic2 de la voie
-  ccoriv, -- Code rivoli de la voie
-  dnvoiri, -- numéro de voirie
-  dindic, -- indice de répétition de voirie
-  ccopos, -- Code postal
-  dnirpp, -- zone à blanc
-  dqualp, -- Qualité abrégée
-  dnomlp, -- Nom d’usage
-  dprnlp, -- Prénoms associés au nom d’usage
-  jdatnss, -- date de naissance
-  dldnss, -- lieu de naissance
-  epxnee, -- mention du complément
-  dnomcp, -- Nom complément
-  dprncp, -- Prénoms associés au complément
-  topcdi, -- top transalp
-  oriard, -- origine adresse
-  fixard, -- pérennité adresse
-  datadr, -- date adresse
-  topdec, -- origine décès
-  datdec, -- date de décès
-  dsiren, -- numéro siren
-  ccmm, -- création compte cadastral
-  topja, -- indic jeune agriculteur
-  datja, -- date jeune agriculteur
-  filler1,
-  anospi, -- ano transalp
-  cblpmo, -- code blocage caractère personne morale
-  gtodge, -- top appartenance à la DGE
-  gpctf, -- top paiement centralisé à la taxe foncière
-  gpctsb, -- top paiement centralisé à la TSBCS
-  jmodge, -- mois d’entrée à la DGE
-  jandge, -- année d’entrée à la DGE
-  jantfc, -- année d’entrée paiement TF
-  jantbc, -- année d’entrée paiement TSBCS
-  filler2
-from prop_article_courant
-left join liste_cgroup on prop_article_courant.cgroup = liste_cgroup.cgroup
-left join liste_ccodro on prop_article_courant.ccodro = liste_ccodro.ccodro
-left join liste_ccodem on prop_article_courant.ccodem = liste_ccodem.ccodem
-left join liste_gtoper on prop_article_courant.gtoper = liste_gtoper.gtoper
-left join liste_ccoqua on prop_article_courant.ccoqua = liste_ccoqua.ccoqua
-left join liste_ccogrm on prop_article_courant.ccogrm = liste_ccogrm.ccogrm
-WHERE "ddenom" LIKE '%COMMUNE DE LA ROCHELLE%';
-
-CREATE VIEW ex_bati_article_00 AS
+-- Parcelles
+drop view if exists dgfip_parcelle;
+CREATE VIEW dgfip_parcelle AS
 select
-  ccodep, -- code département
-  ccodir, -- code direction
-  ccocom, -- code commune INSEE
-  invar, -- numéro invariant
-  --filler01,
-  cenr, -- code enregistrement
-  --filler02,
-  --ccopre, -- préfixe de section ou quartier servi pour les communes associées,
-  ccosec, -- lettres de section
-  dnupla, -- numéro de plan
-  --filler03,
-  dnubat, -- lettre de bâtiment
-  "desc", -- numéro d’entrée
-  dniv, -- niveau étage
-  dpor, -- numéro de local
-  ccoriv, -- Code Rivoli de la voie
-  --filler04,
-  ccovoi, -- Code Majic2 de la voie
-  dnvoiri, -- Numéro de voirie
-  dindic, -- indice de répétition
-  --ccocif, -- code du cdi/cdif (code topad)
-  dvoilib, -- libelle de la voie
-  cleinvar--, -- clé alpha no invariant
-  --locinc--, -- code local sans évaluation
-  --filler05
+  concat(cgroup, dnumcp) as "numero_communal",
+  dnuper as "prop_numero",
+  ddenom as "prop_nom",
+  --nbat_article_10.ccocom as pbd_commune,
+  nbat_article_10.ccosec as pbd_section,
+  ltrim(nbat_article_10.dnupla, '0') as pbd_numero,
+  --concat(nbat_article_10.ccocom, ' ', nbat_article_10.ccosec, ' ', Ltrim(nbat_article_10.dnupla, '0')) as pbd_parcelle,
+  ltrim(nbat_article_10.dnvoiri, '0') as pbd_numero_voirie,
+  concat(nbat_article_10.cconvo, nbat_article_10.dvoilib) as pbd_adresse,
+  nbat_article_10.ccoriv as pbd_code_rivoli--,
+  --*
+from nbat_article_10
+inner join prop_article_courant on concat(prop_article_courant.cgroup, prop_article_courant.dnumcp) = nbat_article_10.dnupro
+--WHERE prop_article_courant.ddenom like '%COMMUNE DE LA ROCHELLE%'
+--WHERE prop_article_courant.ddenom like '%GAREL/FREDERIC%'
+order by
+  pbd_section,
+  cast(nbat_article_10.dnupla as int),
+;
+
+-- Locaux
+drop view if exists dgfip_local;
+CREATE VIEW dgfip_local AS
+select
+  concat(cgroup, dnumcp) as "numero_communal",
+  dnuper as "prop_numero",
+  ddenom as "prop_nom",
+  --nbat_article_10.ccocom as pbd_commune,
+  nbat_article_10.ccosec as pbd_section,
+  ltrim(nbat_article_10.dnupla, '0') as pbd_numero,
+  --concat(nbat_article_10.ccocom, ' ', nbat_article_10.ccosec, ' ', Ltrim(nbat_article_10.dnupla, '0')) as pbd_parcelle,
+  ltrim(nbat_article_10.dnvoiri, '0') as pbd_numero_voirie,
+  concat(bati_article_00.dvoilib) as pbd_adresse,
+  nbat_article_10.ccoriv as pbd_code_rivoli,
+  bati_article_00.dnubat as pbi_bat, -- Numéro du batiment
+  bati_article_00.desc as pbi_ent, -- Numéro d'entrée / d'escalier
+  bati_article_00.dniv as pbi_niv, -- Numéro du niveau
+  bati_article_00.dpor as pbi_porte, -- Numéro de la porte
+  --bati_article_00.invar as pbi_invar,
+  --bati_article_00.cleinvar as pbi_cleinvar,
+  concat(substring(bati_article_00.invar from 4),bati_article_00.cleinvar) as pbi_invariant--,
+  --bati_article_21.dnupev as pbe_pev--,
+  --*
 from bati_article_00
-limit 100;
+  join nbat_article_10 on (bati_article_00.ccosec = nbat_article_10.ccosec and bati_article_00.dnupla = nbat_article_10.dnupla)
+  join prop_article_courant on concat(prop_article_courant.cgroup, prop_article_courant.dnumcp) = nbat_article_10.dnupro
+  left join bati_article_10 on bati_article_00.invar = bati_article_10.invar
+--WHERE prop_article_courant.ddenom like '%COMMUNE DE LA ROCHELLE%'
+--WHERE prop_article_courant.ddenom like '%GAREL/FREDERIC%'
+--WHERE prop_article_courant.ddenom like '%COPROP IMM 7B RUE DES FONDERIES%'
+order by
+  pbd_section,
+  cast(nbat_article_10.dnupla as int),
+  pbi_bat,
+  pbi_ent,
+  pbi_niv,
+  pbi_porte
+;
 
-CREATE VIEW ex_bati_article_10 AS
-SELECT
-  ccodep, -- code département
-  ccodir, -- code direction
-  ccocom, -- code commune INSEE
-  invar, -- numéro invariant
-  --filler01,
-   cenr, -- code enregistrement
-  --filler02,
-  gpdl, -- indicateur d’appartenance à un lot de pdl
-  dsrpar, -- lettre de série rôle
-  dnupro, -- compte communal de propriétaire
-  jdatat, -- date d’acte de mutation
-  dnufnl, -- compte communal de fonctionnaire logé
-  ccoeva_signification, -- code évaluation
-  ccitlv, -- local imposable à la taxe sur les locaux vacants
-  dteloc_signification, -- type de local
-  gtauom, -- zone  de ramassage des ordures ménagères
-  dcomrd, -- Pourcentage de réduction sur tom
-  ccoplc_signification, -- Code de construction particulière
-  cconlc_signification, -- Code nature de local
-  dvltrt, -- Valeur locative totale retenue pour le local
-  ccoape, -- Code NAF pour les locaux professionnels
-  cc48lc, -- Catégorie de loi de 48
-  dloy48a, -- Loyer de 48 en valeur de l’année
-  top48a, -- top taxation indiquant si la pev est impose au loyer ou a la vl
-  dnatlc_signification, -- Nature d occupation
-  dnupas, -- no passerelle TH/TP
-  gnexcf, -- code nature exo ecf
-  dtaucf, -- taux exo ecf
-  cchpr, -- Top indiquant une mutation propriétaire
-  jannat, -- Année de construction
-  dnbniv, -- Nombre de niveaux de la construction
-  hlmsem, -- Local appartenant à hlm ou sem
-  postel, -- Local de Poste ou France Telecom
-  dnatcg, -- Code  nature du changement évaluation
-  jdatcgl, -- date constatation changement
-  dnutbx, -- no gestionnaire déclarant taxe bureaux
-  dvltla, -- VL totale du local actualisée
-  janloc, -- Année de création du local
-  ccsloc, -- Code cause création du local
-  fburx, -- Indicateur présence bureaux
-  gimtom, -- Indicateur imposition OM exploitable à partir de 2002
-  cbtabt, -- Code exonération HLM zone sensible
-  jdtabt, -- Année début d’exonération ZS
-  jrtabt, -- Année fin d’exonération ZS
-  cconac, -- Code NACE pour les locaux professionnels
-  filler03
-from bati_article_10
-left join liste_ccoeva on bati_article_10.ccoeva = liste_ccoeva.ccoeva
-left join liste_dteloc on bati_article_10.dteloc = liste_dteloc.dteloc
-left join liste_ccoplc on bati_article_10.ccoplc = liste_ccoplc.ccoplc
-left join liste_cconlc on bati_article_10.cconlc = liste_cconlc.cconlc
-left join liste_dnatlc on bati_article_10.dnatlc = liste_dnatlc.dnatlc
-limit 100;
 
-CREATE VIEW ex_bati_article_21 AS
-SELECT
+-- pev de la rochelle
+drop view if exists dgfip_pev;
+CREATE VIEW dgfip_pev AS
+select
+  concat(cgroup, dnumcp) as "numero_communal",
+  dnuper as "prop_numero",
+  ddenom as "prop_nom",
+  --nbat_article_10.ccocom as pbd_commune,
+  nbat_article_10.ccosec as pbd_section,
+  ltrim(nbat_article_10.dnupla, '0') as pbd_numero,
+  --concat(nbat_article_10.ccocom, ' ', nbat_article_10.ccosec, ' ', Ltrim(nbat_article_10.dnupla, '0')) as pbd_parcelle,
+  ltrim(nbat_article_10.dnvoiri, '0') as pbd_numero_voirie,
+  concat(bati_article_00.dvoilib) as pbd_adresse,
+  nbat_article_10.ccoriv as pbd_code_rivoli,
+  bati_article_00.dnubat as pbi_bat, -- Numéro du batiment
+  bati_article_00.desc as pbi_ent, -- Numéro d'entrée / d'escalier
+  bati_article_00.dniv as pbi_niv, -- Numéro du niveau
+  bati_article_00.dpor as pbi_porte, -- Numéro de la porte
+  --bati_article_00.invar as pbi_invar,
+  --bati_article_00.cleinvar as pbi_cleinvar,
+  concat(substring(bati_article_00.invar from 4),bati_article_00.cleinvar) as pbi_invariant,
+  --bati_article_21.dnupev as pbe_pev--,
+  bati_article_21.dnupev as pbe_pev,
+  bati_article_21.ccostb as pbe_lettre_serie_tarif, -- Lettre de série tarif bati
+  bati_article_10.ccoeva as pbe_methode_evaluation, -- Methode evaluation
+  ccoaff_signification as pbe_affectation, -- Affectation de la pev
+  --cconad_signification as pbe_nature_local, -- Nature de dépendance
+  bati_article_40.cconad1, -- Nature de dépendance
+  bati_article_21.dcapec as pbe_categorie, -- Categorie
   *
-FROM bati_article_21
-limit 100;
-
-CREATE VIEW ex_bati_article_30 AS
-SELECT
-  *
-FROM bati_article_30
-limit 100;
-
-CREATE VIEW ex_bati_article_36 AS
-SELECT
-  *
-FROM bati_article_36
-limit 100;
-
-
-CREATE VIEW ex_bati_article_40 AS
-SELECT
-  *
-FROM bati_article_40
-limit 100;
-
-CREATE VIEW ex_bati_article_50 AS
-SELECT
-  *
-FROM bati_article_50
-limit 100;
-
-CREATE VIEW ex_bati_article_60 AS
-SELECT
-  *
-FROM bati_article_60
-limit 100;
-
-
-
-CREATE VIEW local AS
-SELECT
-  bati_article_00.*,
-  bati_article_10.*
-FROM bati_article_00
-left join bati_article_10 on bati_article_00.invar = bati_article_10.invar
-WHERE bati_article_10."ccosec" == 'CH'
-  AND bati_article_10."dnupla" == "0385";
-
-CREATE VIEW pev AS
-SELECT
-  bati_article_21.*
-FROM bati_article_21
-limit 100;
+from bati_article_00
+  join nbat_article_10 on (bati_article_00.ccosec = nbat_article_10.ccosec and bati_article_00.dnupla = nbat_article_10.dnupla)
+  join prop_article_courant on concat(prop_article_courant.cgroup, prop_article_courant.dnumcp) = nbat_article_10.dnupro
+  left join bati_article_10 on bati_article_00.invar = bati_article_10.invar
+  left join bati_article_21 on bati_article_00.invar = bati_article_21.invar
+  left join bati_article_40 on bati_article_00.invar = bati_article_40.invar
+  left join liste_ccoaff on bati_article_21.ccoaff = liste_ccoaff.ccoaff
+--WHERE prop_article_courant.ddenom like '%COMMUNE DE LA ROCHELLE%'
+--WHERE prop_article_courant.ddenom like '%GAREL/FREDERIC%'
+--WHERE prop_article_courant.ddenom like '%COPROP IMM 7B RUE DES FONDERIES%'
+order by
+  pbd_section,
+  cast(nbat_article_10.dnupla as int),
+  pbi_bat,
+  pbi_ent,
+  pbi_niv,
+  pbi_porte
+;
 
 
