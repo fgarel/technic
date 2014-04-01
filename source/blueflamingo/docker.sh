60ae818416128b19b5d0dd9b3f77ef374018b573
Frédéric Garel <frederic.garel@gmail.com>
2014-04-01 17:47:08 +0200

    essai docker

---
+++ source/blueflamingo/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#documentation sur docker
+#http://linuxfr.org/news/docker-tutoriel-pour-manipuler-les-conteneurs
+
+#Installation de Docker
+sudo echo "deb http://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+#apt-get update
+#apt-get install lxc-docker
