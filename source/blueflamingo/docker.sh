619cccf4a9728133cec058abbc8f09a0b7ee70c1
Frédéric Garel <frederic.garel@gmail.com>
2014-04-10 23:22:08 +0200

    ajout d un repertoire notebook

--- source/blueflamingo/docker.sh
+++ source/blueflamingo/docker.sh
@@ -4,7 +4,12 @@
 #http://linuxfr.org/news/docker-tutoriel-pour-manipuler-les-conteneurs
 
 #Installation de Docker
-sudo echo "deb http://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+#sudo echo "deb http://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 #apt-get update
 #apt-get install lxc-docker
+
+
+#utilisation de docker
+docker pull debian
+docker run -i -t debian /bin/bash
