34a16cbcd6571b12d8a1631b28a730b0bdeb2d90
Frédéric Garel <frederic.garel@gmail.com>
2014-06-26 15:18:50 +0200

    mbtiles.sh

--- source/gdal/mbtiles.sh
+++ source/gdal/mbtiles.sh
@@ -1,148 +1,65 @@
 #!/bin/sh
 #
 
-# creation d'une image de dimension 1024x1024
-# utilisation d'imageMagick
-#rm logo_tmp.png
-#convert logo: -geometry 1365x1024 logo_tmp.png
-#identify logo_tmp.png
-#convert -gravity center logo_tmp.png -crop 1024x1024+0+0 +repage logo0.png
-#identify logo0.png
-#rm logo_tmp.png
-#rm logo0.png
+#mkdir mypyramid
+#gdal_retile.py -v \
+#               -r bilinear \
+#               -levels 4 \
+#               -co "TILED=YES" \
+#               -co "COMPRESS=JPEG" \
+#               -targetDir mypyramid \
+#               ~/f/CARTOGRAPHIE/donnees/raster/ortho_2013/ortho_2013_lr_cc46.tif
 
-#rm logo_3946.png
-convert -gravity center logo: -geometry 1365x1024 -crop 1024x1024+0+0 +repage logo_3946.png
 
-# le fichier généré est de type 8-bit (????)
-# info
-#identify logo_3946.png
-#gdalinfo logo_3946.png
+#               -ps 2048 2048 \
 
-# creation d'un fichier worldfile (cc46)
-#rm logo_3946.pgw
-cat <<EOF > logo_3946.pgw
-0.1
-0
-0
--0.1
-1379241
-5227395
-EOF
 
-# creation d'un dessin
-# la documentation sur image magick :
-#   les chemins SVG sous image magick
-#   http://www.imagemagick.org/Usage/draw/#paths
-#   les couleurs sous image magick
-#   http://www.imagemagick.org/script/color.php
-# le principe du dessin :
-#   dans un canvas de 1000x1000,
-#   on dessine, avec des instructions tirées du langage svg,
-#   en utilisant des couleurs avec transparence,
-#   dans l'ordre suivant :
-#     on dessine la parcelle avec un trait gris et une couleur de remplissage jaune
-#     on dessine ensuite le batiment en orange avec un coutour orange plus foncé
-#     on redessine la parcelle avec un remplissage transparent
-#rm maison_3946.png
-convert -size 1000x1000 \
-        xc:white \
-        -alpha transparent \
-        -fill '#FFF68F7F' \
-        -stroke '#7F7F7F' \
-        -draw "path 'M 100,900 500,900 500,700 900,100 700,100 250,700 Z'" \
-        -fill '#FFA5007F' \
-        -stroke '#CD8500' \
-        -draw "path 'M 100,900 500,900 500,700 250,700 Z'" \
-        -fill '#FFF68F00' \
-        -stroke '#7F7F7F' \
-        -strokewidth 3 \
-        -draw "path 'M 100,900 500,900 500,700 900,100 700,100 250,700 Z'" \
-        maison_3946.png
+##extraction d'une fenetre
+# -projwin xmin ymax xmax ymin
+# -projwin xHG yHG xBD yBD
+# maison
+# -projwin 1378730 5227900 1379730 5226900
+# maison2
+# -projwin 1378730 5227900 1379730 5226900
+# papi
+# -projwin 1378568 5225902 1380038 5225237
 
-# creation d'un fichier worldfile (cc46)
-#rm maison_3946.pgw
-cat <<EOF > maison_3946.pgw
-0.1
-0
-0
--0.1
-1379241
-5227395
-EOF
+rm ~/geodata/raster/extraction_papi_lr.tif
+rm ~/geodata/raster/extraction_papi_lr.jpg
+#rm ~/geodata/raster/extraction_papi_lr_300.png
 
-# info
-identify maison_3946.png
-#gdalinfo maison_3946.png
+gdal_translate -projwin 1378500 5226000 1380100 5225200 \
+                ../mapnik/vue_2013/ortho_2013_lr_cc46.jp2 \
+                ~/geodata/raster/extraction_papi_lr.tif
+echo "############# apres gdal_translate"
+identify -verbose \
+         ~/geodata/raster/extraction_papi_lr.tif
 
-# recopie du fichier de calage
-rm maison_3946-flatten.pgw
-cp maison_3946.pgw maison_3946-flatten.pgw
+rm ~/geodata/raster/extraction_papi_lr.jpg 
+convert -units PixelsPerInch \
+        -density 300 \
+        ~/geodata/raster/extraction_papi_lr.tif \
+        ~/geodata/raster/extraction_papi_lr.jpg
+echo "############# apres convert jpg pixelsperinch"
+identify -verbose \
+         ~/geodata/raster/extraction_papi_lr.jpg         
 
-# applatissement pour supprimer la transparence
-convert -flatten maison_3946.png maison_3946-flatten.png
+#exiftool -g \
+#         -Photoshop:XResolution=300 \
+#         -Photoshop:DisplayedUnitsX=1 \
+#         -Photoshop:XResolution=300 \
+#         -Photoshop:DisplayedUnitsY=1 \
+#         ~/geodata/raster/extraction_papi_lr.png
+#echo "############# apres exiftool"
+#identify -verbose \
+#         ~/geodata/raster/extraction_papi_lr.png
+         
 
-# info
-identify maison_3946-flatten.png
-#gdalinfo maison_3946-flatten.png
+#rm ~/geodata/raster/extraction_papi_lr_300.png
+#convert  \
+#        -density 300 \
+#        -resample 300 \
+#        ~/geodata/raster/extraction_papi_lr.tif \
+#        ~/geodata/raster/extraction_papi_lr_300.png
 
-# suppression des fichiers avec transparence,
-# a partir de maintenant, nous travaillons avec des fichiers aplatis
-#rm maison_3946.png
-#rm maison_3946.pgw
 
-# transformation du système de coordonnées
-# l'image resultante est en geotif
-rm maison_3857.tfw
-rm maison_3857.tif
-gdalwarp -s_srs 'EPSG:3946' \
-         -t_srs 'EPSG:3857' \
-         -of GTiff \
-         -co 'TFW=yes'\
-         maison_3946.png \
-         maison_3857.tif
-
-# suppression des fichiers png
-rm maison_3946-flatten.png
-rm maison_3946-flatten.pgw
-
-# info
-identify maison_3857.tif
-#gdalinfo maison_3857.tif
-
-# conversion de tif vers png
-rm maison_3857.png
-rm maison_3857.wld
-gdal_translate -of png \
-               -co 'WORLDFILE=yes' \
-               maison_3857.tif \
-               maison_3857.png
-
-rm maison_3857.png
-convert maison_3857.tif maison_3857.png
-
-# suppression des fichiers tif
-#rm maison_3857.tfw
-#rm maison_3857.tif
-
-# info
-identify maison_3857.png
-#gdalinfo maison_3857.png
-
-# creation d'une mosaique
-rm -r tuiles
-gdal2tiles.py -p mercator \
-              -s 'EPSG:3857' \
-              -t "logo" \
-              maison_3857.png \
-              tuiles
-
-# transformation en mbtiles
-wget -O raster2mb https://raw.githubusercontent.com/crschmidt/raster2mb/master/raster2mb
-chmod +x raster2mb
-
-rm maison_3857.mbtiles
-./raster2mb maison_3857.png \
-            maison_3857.mbtiles
-
-rm tmp.png
