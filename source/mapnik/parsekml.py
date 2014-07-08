1ced4cdb625f9761b1abd11f9a8a4e1ef4af7726
Frédéric Garel <frederic.garel@gmail.com>
2014-07-09 00:02:51 +0200

    mapnik : parsekml.py

--- source/mapnik/parsekml.py
+++ source/mapnik/parsekml.py
@@ -12,12 +12,11 @@ facilitant la génération automatique de cartes.
 import os
 from lxml import etree
 from pykml import parser
+from io import StringIO
 #import pykml
 
 
 SITE_FILE = 'PatrimoineVLR.kml'
-
-
 
 
 class Fgmapnik(object):
@@ -29,35 +28,39 @@ class Fgmapnik(object):
 
     """
 
-    def __init__(self, Height=256,
-                       Width=256,
-                       XCenterCC46=1379337.72,
-                       YCenterCC46=5225674.09):
+    def __init__(self,
+                 Height=256,
+                 Width=256,
+                 XCenterCC46=1379337.72,
+                 YCenterCC46=5225674.09):
         u"""
         Initialisation de l'instance de l'objet.
 
         """
         self._site_file = SITE_FILE
 
-
     def parsekml(self):
-        kml_file = os.path.join( \
-            './', \
-            self._site_file)
+        kml_file = os.path.join('./',
+                                self._site_file)
 
         with open(kml_file) as f:
             doc = parser.parse(f)
             root = doc.getroot()
-            print root.Document.name
-            #print etree.tostring(doc, pretty_print=True)
-            #print etree.tostring(doc, pretty_print=False)
-            #print root.Document.Folder.Placemark
-            for element in root.Document.Folder.iter("*"):
-                print root.Document.Folder.Placemark.Point.coordinates
-            for event, element in etree.iterparse(etree.tostring(doc)):
-                print("%s, %4s, %s" % (event, element.tag, element.text))
- 
- 
+            #print("root = %s" % (etree.tostring(root, pretty_print=True)))
+            context = etree.iterwalk(root, tag="*")
+            #context = etree.iterwalk(root)
+            count = 0
+            for event, element in context:
+                #print("%s,tag='%4s', %s" % (event, element.tag, element.text))
+                if element.tag == '{http://www.opengis.net/kml/2.2}name':
+                    name = element.text
+                if element.tag == '{http://www.opengis.net/kml/2.2}coordinates':
+                    count += 1
+                    x, y, z = element.text.split(',')
+                    print("Point= %s, %s, X= %s, Y= %s, Z= %s" % (name, element.text, x, y, z))
+            print count
+
+
 if __name__ == '__main__':
 
     # Instanciation d'un objet (site avec des coordonnées)
@@ -66,6 +69,6 @@ if __name__ == '__main__':
                              Width=256,
                              XCenterCC46=1379230,
                              YCenterCC46=5227400)
-    
+
     # Creation des différentes cartes pour cet objet
     _mon_fgmapnik.parsekml()
