954888c799c717fe102f00f58798c8a1679801b1
Frédéric Garel <frederic.garel@gmail.com>
2014-11-23 22:38:27 +0100

    docker run

--- source/blueflamingo/develop/dockrunzsh.sh
+++ source/blueflamingo/develop/dockrunzsh.sh
@@ -2,7 +2,11 @@
 
 
 # pour travailler dans la nouvelle image blueflamingo
-docker.io run -i -t -u blueflamingo \
+docker.io run -i \
+              -t \
+              --name="myContainer" \
+              -e "TERM=screen-256color" \
+              -u blueflamingo \
               -w /home/blueflamingo \
               -e "HOME=/home/blueflamingo" \
               fgarel/blueflamingo:1.0 /bin/zsh
