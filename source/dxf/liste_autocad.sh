d0218d8a38025ce3644d4ce627580c50c1a4f2a0
Frédéric Garel <frederic.garel@gmail.com>
2015-02-13 17:39:42 +0100

    liste_autocad

--- source/dxf/liste_autocad.sh
+++ source/dxf/liste_autocad.sh
@@ -4,13 +4,23 @@ find /mnt/dsifichiers02 \
      -regextype posix-extended \
      -iregex '.*\.(dxf|dwg)' \
      | \
-     egrep -v -i -E '/biblio/' \
-     | \
-     egrep -v -i -E '/Blocs autocad/' \
+     egrep -v -i -E '/(biblio|blocs?)( ?|_)(autocad|pamcad|th.ques?)?/' \
      | \
      egrep -v -i -E '/PAMCAD/' \
      | \
      egrep -v -i -E '/forster CAD_EXP_F/' \
+     | \
+     egrep -v -i -E '/LTB2000 OEM/' \
+     | \
+     egrep -v -i -E '/Biblioth.que_architecture/' \
+     | \
+     egrep -v -i -E '/ARBRES-HO-FE-VEHICULES/' \
+     | \
+     egrep -v -i -E '/MATHIEU MARIN/' \
+     | \
+     egrep -v -i -E '/BANQUE_IMAGE/' \
+     | \
+     egrep -v -i -E '/VLR/Symb2d/' \
     > /home/fred/Documents/liste_autocad.txt
 
 
@@ -18,16 +28,8 @@ find /mnt/dsifichiers02 \
      -regextype posix-extended \
      -iregex '.*\.(dxf|dwg)' \
      | \
-     egrep -i -E '/biblio/' \
+     egrep -i -E '/(biblio|blocs?)( ?|_)(autocad|pamcad|th.ques?)?/' \
     > /home/fred/Documents/liste_autocad_bibliotheque.txt
-
-find /mnt/dsifichiers02 \
-     -regextype posix-extended \
-     -iregex '.*\.(dxf|dwg)' \
-     | \
-     egrep -i -E '/Blocs autocad/' \
-     | \
-    >> /home/fred/Documents/liste_autocad_bibliotheque.txt
 
 find /mnt/dsifichiers02 \
      -regextype posix-extended \
@@ -42,3 +44,46 @@ find /mnt/dsifichiers02 \
      | \
      egrep -i -E '/forster CAD_EXP_F/' \
     >> /home/fred/Documents/liste_autocad_bibliotheque.txt
+
+find /mnt/dsifichiers02 \
+     -regextype posix-extended \
+     -iregex '.*\.(dxf|dwg)' \
+     | \
+     egrep -i -E '/LTB2000 OEM/' \
+    >> /home/fred/Documents/liste_autocad_bibliotheque.txt
+
+find /mnt/dsifichiers02 \
+     -regextype posix-extended \
+     -iregex '.*\.(dxf|dwg)' \
+     | \
+     egrep -i -E '/Biblioth.que_architecture/' \
+    >> /home/fred/Documents/liste_autocad_bibliotheque.txt
+
+find /mnt/dsifichiers02 \
+     -regextype posix-extended \
+     -iregex '.*\.(dxf|dwg)' \
+     | \
+     egrep -i -E '/ARBRES-HO-FE-VEHICULES/' \
+    >> /home/fred/Documents/liste_autocad_bibliotheque.txt
+
+find /mnt/dsifichiers02 \
+     -regextype posix-extended \
+     -iregex '.*\.(dxf|dwg)' \
+     | \
+     egrep -i -E '/MATHIEU MARIN/' \
+    >> /home/fred/Documents/liste_autocad_bibliotheque.txt
+
+find /mnt/dsifichiers02 \
+     -regextype posix-extended \
+     -iregex '.*\.(dxf|dwg)' \
+     | \
+     egrep -i -E '/BANQUE_IMAGE/' \
+    >> /home/fred/Documents/liste_autocad_bibliotheque.txt
+
+find /mnt/dsifichiers02 \
+     -regextype posix-extended \
+     -iregex '.*\.(dxf|dwg)' \
+     | \
+     egrep -i -E '/VLR/Symb2d/' \
+    >> /home/fred/Documents/liste_autocad_bibliotheque.txt
+
