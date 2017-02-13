146c131eee3cc37df87407903db3dbe4c74fd9d6
Frédéric Garel <frederic.garel@gmail.com>
2017-02-13 07:54:16 +0100

    poster

--- source/poster/bin/14_imagemagick.py
+++ source/poster/bin/14_imagemagick.py
@@ -7,7 +7,7 @@ import stat
 import re
 
 fichier_a_executer = '15_imagemagick.sh'
-chemin_des_images = '/home/fred/Images/v6/'
+chemin_des_images = '/home/fred/Images/v4/'
 
 
 list_image = os.listdir(chemin_des_images)
@@ -26,6 +26,17 @@ for image in list_image:
         #print('   {}'.format(image))
         #f.write('identify {}.png\n'.format(image))
         #f.write('convert -density 300 -gravity Center -shave \'045x049\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_v2.png\n'.format(chemin_des_images, image))
+        for x in range(254, 257, 2):
+            for y in range(44, 47, 2):
+                #f.write('convert -density 150 -gravity Center -shave \'{2}x{3}\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d150_{2}_{3}.png\n'.format(chemin_des_images, image, x, y))
+                pass
+        for x in range(508, 513, 2):
+            for y in range(88, 93, 2):
+                #f.write('convert -density 300 -gravity Center -shave \'{2}x{3}\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d300_{2}_{3}.png\n'.format(chemin_des_images, image, x, y))
+                pass
+        f.write('convert -density 150 -gravity Center -shave \'256x045\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d150_256_045.png\n'.format(chemin_des_images, image))
+        f.write('convert -density 300 -gravity Center -shave \'512x090\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d300_512_090.png\n'.format(chemin_des_images, image))
+        """
         f.write('convert -density 150 -gravity Center -shave \'045x045\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d150_045_045.png\n'.format(chemin_des_images, image))
         f.write('convert -density 150 -gravity Center -shave \'220x045\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d150_220_045.png\n'.format(chemin_des_images, image))
         f.write('convert -density 150 -gravity Center -shave \'230x045\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d150_230_045.png\n'.format(chemin_des_images, image))
@@ -56,6 +67,7 @@ for image in list_image:
         f.write('convert -density 300 -gravity Center -shave \'525x205\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d300_525_205.png\n'.format(chemin_des_images, image))
         f.write('convert -density 300 -gravity Center -shave \'525x245\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d300_525_245.png\n'.format(chemin_des_images, image))
         f.write('convert -density 300 -gravity Center -shave \'525x265\' -level +0%,+100%,1.5 {0}{1}.pdf {0}{1}_d300_525_265.png\n'.format(chemin_des_images, image))
+        """
 f.close()
 
 st = os.stat(fichier_a_executer)
