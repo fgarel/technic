c62a1096169f86e4b5b9002265d4e23e396b7451
Frédéric Garel <frederic.garel@gmail.com>
2017-06-30 17:29:06 +0200

    ldap

--- source/gup/bin/sync_ldap_atal.sh
+++ source/gup/bin/sync_ldap_atal.sh
@@ -1,18 +1,22 @@
 #/bin/sh
 
-# Recherche des données dans l'annuaire ldap
+# #############################################
+# 1. Recherche des données dans l'annuaire ldap
+
 ldapsearch -h aul.larochelle.fr -x -b "ou=people,dc=larochelle,dc=fr" -LL dn manager employeeNumber > annuaire0.ldif
 ldapsearch -h aul.larochelle.fr -x -b "ou=people,dc=larochelle,dc=fr" > annuaire1.ldif
 
 
-# Suppression des données photos
+# #############################################
+# 2. Suppression des données photos
 # utilisation du site
 # https://regex101.com/codegen?language=python
 #
 # le script photo_remove.py decoule de ce peaufinage
 ./photo_remove.py
 
-# Utilisation d'un utilitaire pour convertir du format ldif pour fabriquer un organigramme
+# #############################################
+# 3. Utilisation d'un utilitaire pour convertir du format ldif pour fabriquer un organigramme
 # http://marcin.owsiany.pl/ldif2dot-page
 #
 #ldapsearch -h aul.larochelle.fr -x -b 'ou=people,dc=larochelle,dc=fr' | \
